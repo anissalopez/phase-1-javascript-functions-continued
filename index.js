6314b51bd4b6f0fe739f4a6234a942dc4d02952a
anissalopez <anissa965@gmail.com>
2023-04-26 14:11:48 -0400

    functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (flair){
+    return  function inner (adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+/* Implement a function called `wrapAdjective`:
+
+- It should return a function. This "inner" function should:
+  - take a single parameter that should default to `"special"`. Name it
+    however you wish.
+  - return a `String` of the form "You are ...!" where `...` should be the
+    adjective this function received wrapped in visual flair.
+- It should take as parameter a `String` that will be used to create visual flair.
+- You may call the parameter whatever you like, but its default value should
+  be `"*"`.
+- Call example: `const encouragingPromptFunction = wrapAdjective("!!!")` */
